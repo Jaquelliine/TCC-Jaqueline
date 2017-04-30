36c0696382fb94c976e59bc3494217cf5e90a550
https://github.com/Jaquelliine/HomeGiver <lopesjaque22@hotmail.com>
2017-04-30 12:42:35 -0300

    atualizações nas telas

--- app/src/main/java/com/example/lenovog40/tcc/CadastroMembroCuidador.java
+++ app/src/main/java/com/example/lenovog40/tcc/CadastroMembroCuidador.java
@@ -1,7 +1,10 @@
 package com.example.lenovog40.tcc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CadastroMembroCuidador extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class CadastroMembroCuidador extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro_membro_cuidador);
+
+        Button cadastroCM = (Button) findViewById(R.id.cadastroCM);
+        cadastroCM.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(CadastroMembroCuidador.this, Dashboard.class);
+                startActivity(i);
+            }
+        });
+
+
+
+
     }
 }
