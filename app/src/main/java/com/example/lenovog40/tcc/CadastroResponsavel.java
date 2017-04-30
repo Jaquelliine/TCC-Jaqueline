b8e3df9a9cd3ea8130777a737cabe109042b638c
https://github.com/Jaquelliine/HomeGiver <lopesjaque22@hotmail.com>
2017-04-30 12:37:03 -0300

    atualizações nas telas

--- app/src/main/java/com/example/lenovog40/tcc/CadastroResponsavel.java
+++ app/src/main/java/com/example/lenovog40/tcc/CadastroResponsavel.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class CadastroResponsavel extends AppCompatActivity {
 
@@ -15,6 +16,18 @@ public class CadastroResponsavel extends AppCompatActivity {
 
         Button cadastroR = (Button) findViewById(R.id.cadastroR);
         cadastroR.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(CadastroResponsavel.this, Login.class);
+                startActivity(i);
+            }
+        });
+
+
+
+        TextView logar = (TextView) findViewById(R.id.logar);
+        logar.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/lenovog40/tcc/Login.java
+++ app/src/main/java/com/example/lenovog40/tcc/Login.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
@@ -21,6 +22,21 @@ public class Login extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        TextView criarContaR = (TextView) findViewById(R.id.criarContaR);
+
+        criarContaR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Login.this, CadastroResponsavel.class);
+                startActivity(i);
+            }
+        });
+
+
+
+
+
         {
 
         }
