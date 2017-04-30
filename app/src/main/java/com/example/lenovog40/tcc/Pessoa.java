7218d27f1ee3025b923fc3acaeaff7061469637a
https://github.com/Jaquelliine/HomeGiver <lopesjaque22@hotmail.com>
2017-04-30 17:34:40 -0300

    atualizações nas telas

---
+++ app/src/main/java/com/example/lenovog40/tcc/Pessoa.java
@@ -0,0 +1,82 @@
+package com.example.lenovog40.tcc;
+
+/**
+ * Created by Lenovo G40 on 30/04/2017.
+ */
+
+public class Pessoa {
+
+    int _id;
+    String nome;
+    double telefone =0;
+    String endereco;
+    String email;
+    String senha;
+    int cpf =0;
+    boolean nivelAcesso;
+
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+
+    public double getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(double telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public boolean isNivelAcesso() {
+        return nivelAcesso;
+    }
+
+    public void setNivelAcesso(boolean nivelAcesso) {
+        this.nivelAcesso = nivelAcesso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
